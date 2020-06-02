415863792bb6bcbdfa8fd551c6b2fcbaded93a50
phtf92 <phtf92@gmail.com>
2020-06-02 19:17:22 -0300

    Updates

---
+++ precache-manifest.afe1b99e44147d2d5d5f15ecb7214b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2302435403a615fbb12dedabc25337d5",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "a5ec0b162adb29618cd4",
+    "url": "/trivia-react-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8e315c66c792c4554d7",
+    "url": "/trivia-react-redux/static/js/2.78306db8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trivia-react-redux/static/js/2.78306db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ec0b162adb29618cd4",
+    "url": "/trivia-react-redux/static/js/main.e980458e.chunk.js"
+  },
+  {
+    "revision": "ec1badd426d8c77da2f8",
+    "url": "/trivia-react-redux/static/js/runtime-main.30dec5aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trivia-react-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
